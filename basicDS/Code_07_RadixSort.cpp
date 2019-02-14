eb144af5709f30100441364065d689e760ff41d0
guo <guochangyounihao@163.com>
2019-02-15 00:02:41 +0800

    add BucketSort

--- basicDS/Code_07_RadixSort.cpp
+++ basicDS/Code_07_RadixSort.cpp
@@ -6,37 +6,18 @@
 #include <string.h>
 
 /*
-    video 3; 1:10:00
-    BucketSort 与数值状况有关
-    在某些状况下，很快，例如：最大值比较小。
-
+    仅使用于非负值
 */
 class Code_00 {
 public:
-    void BucketSort(int arr[], int* size) {
+    void RadixSort(int arr[], int* size) {
         int len = *size;
         if(arr == NULL && len < 2) {
             return;
         }
-        int maxValue = INT_MIN;
-        for(int i=0; i<len; i++) {
-            if(arr[i] > maxValue) {
-                maxValue = arr[i];
-            }
-        }
 
-        int* bucket = (int*) malloc ((maxValue+1)*sizeof(int));
-        memset(bucket, 0, (maxValue+1)*sizeof(int));
-        for(int i=0; i<len; i++) {
-            bucket[arr[i]]++;
-        }
 
-        int i=0;
-        for(int j=0; j<maxValue+1; j++) {
-            while(bucket[j]-->0) {
-                arr[i++] = j;
-            }
-        }
+
 
     }
 
@@ -110,7 +91,7 @@ public:
         for(int i=0; i<testTime; i++) {
             int* arr1 = generateRandomArray(maxSize, maxValue, ActualSize);
             int* arr2 = copyArray(arr1, ActualSize);
-            BucketSort(arr1, ActualSize);
+            RadixSort(arr1, ActualSize);
             comparator(arr2, ActualSize);
             if( !isEqual(arr1, arr2, ActualSize)) {
                 succeed = 0;
@@ -125,7 +106,7 @@ public:
 
         int* arr = generateRandomArray(maxSize, maxValue, ActualSize);
         printArray(arr, ActualSize);
-        BucketSort(arr, ActualSize);
+        RadixSort(arr, ActualSize);
         printArray(arr, ActualSize);
         free(arr);
     }
