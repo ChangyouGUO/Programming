f79bcb16d56f352b8b2b19849a24392ded5387f1
guo <guochangyounihao@163.com>
2019-02-16 23:37:36 +0800

    add RadixSort

--- basicDS/Code_07_RadixSort.cpp
+++ basicDS/Code_07_RadixSort.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <limits.h>
 #include <string.h>
+#include <math.h>
 
 /*
     仅使用于非负值
@@ -15,11 +16,58 @@ public:
         if(arr == NULL && len < 2) {
             return;
         }
+        RadixSort(arr, 0, len-1, maxbits(arr, len));
+    }
 
+    int maxbits(int arr[], int len) {
+        int maxValue = INT_MIN;
+        for(int i=0; i<len; i++) {
+            if(arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+        int bits = 0;
+        while(maxValue != 0) {
+            maxValue /= 10;
+            bits++;
+        }
+        return bits;
+    }
 
+    void RadixSort(int arr[], int begin, int end, int bits) {
+        int radix = 10;
+        int i, j;
+        int* count = (int*) malloc(radix*sizeof(int));
+        memset(count, 0, radix*sizeof(int));
+        int* bucket = (int*) malloc((end-begin+1)*sizeof(int));
+        memset(bucket, 0, (end-begin+1)*sizeof(int));
 
+        for(int d=1; d<=bits; d++) {
+            // count 清零
+            memset(count, 0, radix*sizeof(int));
+            for(i=begin; i<=end; i++) {
+                j = getbit(arr[i], d);
+                count[j]++;
+            }
+            for(i=1; i<radix; i++) {
+                count[i] += count[i-1];
+            }
+            for(i=end; i>=begin; i--) {
+                j = getbit(arr[i], d);
+                bucket[count[j]-1] = arr[i];  //注意这里bucket中的序号
+                count[j]--;
+            }
+            for(i=begin, j=0; i<=end; i++, j++) {  
+                arr[i] = bucket[j];  //bucket和arr中的序号不同的
+            }
+        }
 
     }
+
+    int getbit(int val, int p) {
+        return (int)(val/pow(10, p-1))%10;
+    }
+
 
     static int cmpfunc(const void* a, const void* b) {
         int* x = (int*) a;   //need new pointers: x, y
