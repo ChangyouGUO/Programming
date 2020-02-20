5f71c3a2b2b9987d41bfb82a5caa7584527639f3
Guo changyou <changyou.guo.metoak.loc>
2020-02-20 18:47:01 +0800

    try to modify drawLine.py

--- Python/drawLine.py
+++ Python/drawLine.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import optimize
 
 # 显示默认的字体库
-# print(matplotlib.matplotlib_fname())
+print(matplotlib.matplotlib_fname())
 
 #显示中文标签
 plt.rcParams['font.sans-serif']=['SimHei'] 
@@ -20,38 +20,44 @@ def main():
     yObjectThresh = offset(xObjectThresh) + nSafePassHeight
     yObjectSafePass = np.ones(90) *nSafePassHeight
     
-    plt.plot(xObjectThresh, yObjectThresh, label='目标高度报警阈值')
-    lineSafe, = plt.plot(xObjectThresh, yObjectSafePass, linestyle='-', label='车辆最低可安全通过高度')
+    fig, ax1 = plt.subplots()
+
+    ax1.plot(xObjectThresh, yObjectThresh, label='目标高度报警阈值')
+    lineSafe, = ax1.plot(xObjectThresh, yObjectSafePass, linestyle='-', label='车辆最低可安全通过高度')
     lineSafe.set_dashes([3, 3])
 
     # draw points
-    xPoints = np.arange(85, 35, -3)
-    yPoints = np.array([4.15, 4.1, 3.9, 3.85, 3.95, 4.1, 4.4, 4.12, 3.95, 4.25, 4.3, 4.25, 4.0, 3.95, 4.2, 4.25, 4.25])
+    yPoints = np.array([5.5, 5, 4.5, 4, 3.5, 4, 4.5, 5, 4.5, 4, 4.3, 4.25, 4.0, 3.95, 4.2, 4.25, 4.25])
+    xPoints = np.arange(85, 85-yPoints.size*2, -2)
 
     # line fit
-    fFitLine = np.polyfit(xPoints, yPoints, 10)
+    fFitLine = np.polyfit(xPoints, yPoints, 2)
     yFitValue = np.polyval(fFitLine, xPoints)
-    plt.plot(xPoints[:3], yFitValue[:3], "b")
-    plt.plot(xPoints[2:11], yFitValue[2:11], "r")
-    plt.plot(xPoints[10:], yFitValue[10:], "b")
+    ax1.plot(xPoints[:3], yFitValue[:3], "b")
+    ax1.plot(xPoints[2:11], yFitValue[2:11], "r")
+    ax1.plot(xPoints[10:], yFitValue[10:], "b")
    
-    plt.scatter(xPoints, yPoints, marker="o", color=["b", "b", "r", "r", "r", "r", "r", "r", "r", "r", "b", "b", "b", "b", "b", "b", "b"], label="目标高度\n蓝色: 不报警\n红色: 报警")
+    ax1.scatter(xPoints, yPoints, marker="o", color=["b", "b", "r", "r", "r", "r", "r", "r", "r", "r", "b", "b", "b", "b", "b", "b", "b"], label="目标高度\n蓝色: 不报警\n红色: 报警")
 
-    plt.annotate("1", xy=(85, 4.15), xytext=(84, 4.3), color="b")
-    plt.annotate("2", xy=(82, 4.1),  xytext=(81, 4.2), color="b")
-    plt.annotate("3", xy=(79, 3.9),  xytext=(78, 3.6), color="r")
-    plt.annotate("4", xy=(67, 4.4),  xytext=(66, 4.53), color="r")
-    plt.annotate("5", xy=(58, 4.25), xytext=(57.3, 4.4), color="r")
-    plt.annotate("6", xy=(55, 4.3), xytext=(54, 4.45), color="b")
-    plt.annotate("7", xy=(49, 4.0), xytext=(48, 3.65), color="b")
-    plt.annotate("8", xy=(46, 3.95), xytext=(45, 3.6), color="b")
+    ax1.annotate("1", xy=(85, 4.15), xytext=(84, 4.3), color="b")
+    ax1.annotate("2", xy=(82, 4.1),  xytext=(81, 4.2), color="b")
+    ax1.annotate("3", xy=(79, 3.9),  xytext=(78, 3.6), color="r")
+    ax1.annotate("4", xy=(67, 4.4),  xytext=(66, 4.53), color="r")
+    ax1.annotate("5", xy=(58, 4.25), xytext=(57.3, 4.4), color="r")
+    ax1.annotate("6", xy=(55, 4.3), xytext=(54, 4.45), color="b")
+    ax1.annotate("7", xy=(49, 4.0), xytext=(48, 3.65), color="b")
+    ax1.annotate("8", xy=(46, 3.95), xytext=(45, 3.6), color="b")
 
 
-    plt.axis([0, 90, 0, 7])
-    plt.xlabel('距离(m)')
-    plt.ylabel('高度(m)')
+
+    ax1.axis([0, 90, 0, 7])
+    ax1.set_xlabel('距离(m)')
+    ax1.set_ylabel('高度(m)')
+
+    ax2 = ax1.twinx()
+    ax2.axis([0, 90, 0, 7])
     
-    plt.legend()
+    ax1.legend()
     plt.show()
 
 
