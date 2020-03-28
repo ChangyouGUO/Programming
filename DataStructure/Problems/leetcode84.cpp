656d3e0fda07c77dcceb35b239935f9979a3f3a9
guochangyou <guochangyounihao@163.com>
2020-03-29 00:47:33 +0800

    DataStructure: Monotonic stack, leetcode84, leetcode85

--- DataStructure/Problems/leetcode84.cpp
+++ DataStructure/Problems/leetcode84.cpp
@@ -1,3 +1,73 @@
 /*
- Monotonic stack
+  Copyright [2020] <GCY>
+  Monotonic stack
+  计算面积需要两侧边界和高度。
+  考虑只要右侧的数值比左侧开头的值小，此时统计的区域就已经形成。
+  采用单调递减栈(增减性为出栈顺序)
+  leetcode 84
  */
+
+#include <stdio.h>
+
+#include <iostream>
+#include <stack>
+
+// leetcode doesn't give number limit
+#define LEN (100)
+
+// 注意：有效区间不是i - left_index, 应该是i- left_left_index-1
+// 考虑到最后一个值的边界情况，stack 最开始压入一个0
+/*
+int GetMaxArea(int n, int* heights) {
+  std::stack<int> mono_stack;
+  mono_stack.push(-1);
+  int result = 0;
+  int i = 0;
+  for (; i < n; i++) {
+    while (mono_stack.size() > 1 && heights[mono_stack.top()] > heights[i]) {
+      int value = heights[mono_stack.top()];
+      mono_stack.pop();
+      result = std::max(result, (i - mono_stack.top() - 1) * value);
+    }
+    mono_stack.push(i);
+  }
+  while (mono_stack.size() > 1) {
+    int value = heights[mono_stack.top()];
+    mono_stack.pop();
+    result = std::max(result, (i - mono_stack.top() - 1) * value);
+  }
+  return result;
+}
+*/
+
+// 给定数为非负整数，在height中多压如一个0，可以将最后stack剩余变量的情况和遍历时while的情况结合在一起。
+int GetMaxArea(int n, int* heights) {
+  std::stack<int> mono_stack;
+  heights[n] = 0;
+  int result = 0;
+  int i = 0;
+  for (; i <= n; i++) {
+    while (!mono_stack.empty() && heights[mono_stack.top()] > heights[i]) {
+      int value = heights[mono_stack.top()];
+      mono_stack.pop();
+      int left_index = mono_stack.empty() ? -1 : mono_stack.top();
+      result = std::max(result, (i - left_index - 1) * value);
+    }
+    mono_stack.push(i);
+  }
+  return result;
+}
+
+int main() {
+  int n;
+  int heights[LEN];
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &heights[i]);
+  }
+
+  int result = GetMaxArea(n, heights);
+  printf("%d\n", result);
+
+  return 0;
+}
--- DataStructure/Problems/leetcode85.cpp
+++ DataStructure/Problems/leetcode85.cpp
@@ -1,3 +1,64 @@
 /*
+Copyright [2020] <gcy>
   Monotonic stack
+  leetcode85.cpp
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <stack>
+
+#define LEN (1000)
+
+// 复用leetcode84中的代码
+int GetMaxArea(int n, int* heights) {
+  std::stack<int> mono_stack;
+  heights[n] = 0;
+  int result = 0;
+  int i = 0;
+  for (; i <= n; i++) {
+    while (!mono_stack.empty() && heights[mono_stack.top()] > heights[i]) {
+      int value = heights[mono_stack.top()];
+      mono_stack.pop();
+      int left_index = mono_stack.empty() ? -1 : mono_stack.top();
+      result = std::max(result, (i - left_index - 1) * value);
+    }
+    mono_stack.push(i);
+  }
+  return result;
+}
+
+// 将最大矩形面积求解，分解为按行叠加后，直方图最大面积(leetcode84)
+int MaxRectangleArea(int r, int c, int (*array)[LEN]) {
+  int sum_array[LEN];
+  int result = 0;
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      if (0 == array[i][j]) {
+        sum_array[j] = 0;
+      } else {
+        sum_array[j] += 1;
+      }
+    }
+    result = std::max(result, GetMaxArea(c, sum_array));
+  }
+  return result;
+}
+
+int main() {
+  int r = 0;
+  int c = 0;
+  scanf("%d", &r);
+  scanf("%d", &c);
+  int array[LEN][LEN];
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      scanf("%d", &array[i][j]);
+    }
+  }
+  int result = MaxRectangleArea(r, c, array);
+  printf("%d", result);
+  return 0;
+}
